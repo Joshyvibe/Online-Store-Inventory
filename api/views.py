1284696b80ec40240cfc11de95bb755fd2b1ba6c
Obinna Akpulonu <akpulonujoseph@gmail.com>
2025-02-07 23:50:32 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import StoreInventory, Supplier, Item
-from .serializers import StoreInventorySerializer, SupplierSerializer, ItemSerializer
+from .serializers import *
 
 
 
